3d3ff751b579dabff24a2262f64ad7fe95d2ded3
devendra456 <devendramaurya456@gmail.com>
2021-12-27 17:24:31 +0530

    send money screen created

--- lib/app/modules/CreateNewPlan/controllers/create_new_plan_controller.dart
+++ lib/app/modules/CreateNewPlan/controllers/create_new_plan_controller.dart
@@ -4,8 +4,36 @@ import 'package:meal_box/app/modules/register/views/school_dialog_view.dart';
 
 class CreateNewPlanController extends GetxController {
   var selectedRadio = 0.obs;
-
   var viewPagerController = PageController(initialPage: 0);
+  var selectedMonth = null.obs;
+
+  final List<String> listOfMonth = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  final List<String> listOfDays = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+  ];
+
+  DateTime selectedDate = DateTime.now();
+  int currentSelectedDateIndex = 0;
 
   void changeSchoolName() {
     Get.dialog(const SchoolDialogView());
@@ -18,5 +46,9 @@ class CreateNewPlanController extends GetxController {
       duration: const Duration(milliseconds: 200),
       curve: Curves.linear,
     );
+  }
+
+  void onChange(String value) {
+    //selectedMonth.value = value.toString();
   }
 }
--- lib/app/modules/CreateNewPlan/views/daily_plan_view.dart
+++ lib/app/modules/CreateNewPlan/views/daily_plan_view.dart
@@ -1,11 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:meal_box/app/modules/CreateNewPlan/controllers/create_new_plan_controller.dart';
 
-class DailyPlanView extends GetView {
+class DailyPlanView extends GetView<CreateNewPlanController> {
   const DailyPlanView({Key? key}) : super(key: key);
+
+  //CreateNewPlanController controller = Get.find<CreateNewPlanController>();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        /*DropdownButton(
+          items: controller.listOfMonth
+              .map(
+                (e) => DropdownMenuItem(child: Text(e)),
+              )
+              .toList(),
+
+        ),*/
+        DropdownButton<String>(
+          items: controller.listOfMonth.map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (value) {
+            controller.onChange(value!);
+          },
+          value: controller.selectedMonth.value,
+          hint: Text("Please select month"),
+          underline: SizedBox(),
+        ),
+        SizedBox(
+          height: 88,
+          child: ListView.separated(
+            itemBuilder: (context, index) {
+              return Container(
+                width: 72,
+                color: Colors.green,
+                child: Column(
+                  children: [
+                    Text(
+                      controller.listOfMonth[
+                          DateTime.now().add(Duration(days: index)).month - 1],
+                    ),
+                    Text(
+                      DateTime.now().add(Duration(days: index)).day.toString(),
+                    ),
+                    Text(
+                      controller.listOfDays[
+                          DateTime.now().add(Duration(days: index)).weekday -
+                              1],
+                    )
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return const SizedBox(
+                width: 16,
+              );
+            },
+            itemCount: 365,
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            scrollDirection: Axis.horizontal,
+          ),
+        ),
+      ],
+    );
   }
 }
